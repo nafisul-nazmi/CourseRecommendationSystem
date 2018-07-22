3bd8b7ab9ff8687e36cc3f03988fdf4e59ddd9ae
Nafisul Islam Nazmi <nazmi.ndc@gmail.com>
2018-07-22 22:36:27 +0600

    Added models.

---
+++ CRS.Entity/Models/Program.cs
@@ -0,0 +1,22 @@
+﻿using CRS.Entity.Generics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CRS.Entity.Models
+{
+    public class Program : BaseEntity
+    {
+        // Database fields
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public int DepartmentId { get; set; }
+
+        // Navigation properties
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+        public virtual ICollection<ProgramCourseAssociation> ProgramCourseAssociations { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ CRS.Entity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using CRS.Entity.Generics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CRS.Entity.Models
+{
+    public class Student : BaseEntity
+    {
+        // Database fields
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public double CGPA { get; set; }
+        public int ProgramId { get; set; }
+
+        // Navigation fields
+        [ForeignKey("ProgramId")]
+        public virtual Program Program { get; set; }
+        public virtual ICollection<StudentCourseAssociation> StudentCourseAssociations { get; set; }
+    }
+}
