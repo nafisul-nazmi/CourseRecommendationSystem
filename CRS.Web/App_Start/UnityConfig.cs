1d2d93d175ccee6edb201d1ce8c08e2d359d7346
Nafisul Islam Nazmi <nafisul.islam@enosisbd.com>
2018-07-24 11:00:40 +0600

    Hotfix: Unity IOC Container location.

--- CRS.Web/App_Start/UnityConfig.cs
+++ CRS.Web/App_Start/UnityConfig.cs
@@ -1,3 +1,9 @@
+using CRS.BLL.Interfaces;
+using CRS.BLL.Services;
+using CRS.DAL.Contexts;
+using CRS.DAL.Interfaces;
+using CRS.DAL.Repositories;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Unity;
 using Unity.Mvc5;
@@ -8,13 +14,32 @@ namespace CRS.Web
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+            var container = new UnityContainer();
+
+            // Add mappings here
+            // Context
+            container.RegisterType<DbContext, CRSContext>();
+
+            // Repositories
+            container.RegisterType<IAdminRepository, AdminRepository>();
+            container.RegisterType<IDepartmentRepository, DepartmentRepository>();
+            container.RegisterType<IProgramRepository, ProgramRepository>();
+            container.RegisterType<ICourseRepository, CourseRepository>();
+            container.RegisterType<IProgramCourseAssociationRepository, ProgramCourseAssociationRepository>();
+            container.RegisterType<IPrerequisiteRepository, PrerequisiteRepository>();
+            container.RegisterType<IStudentRepository, StudentRepository>();
+            container.RegisterType<IStudentCourseAssociationRepository, StudentCourseAssociationRepository>();
+
+            // Services
+            container.RegisterType<IAdminService, AdminService>();
+            container.RegisterType<IDepartmentService, DepartmentService>();
+            container.RegisterType<IProgramService, ProgramService>();
+            container.RegisterType<ICourseService, CourseService>();
+            container.RegisterType<IProgramCourseAssociationService, ProgramCourseAssociationService>();
+            container.RegisterType<IPrerequisiteService, PrerequisiteService>();
+            container.RegisterType<IStudentService, StudentService>();
+            container.RegisterType<IStudentCourseAssociationService, StudentCourseAssociationService>();
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
